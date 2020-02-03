e65bcae5c4f44d8961a15e9e3ac6ef83fb7ad944
Kai Jennissen <kai.jennissen@statworx.com>
2020-02-03 09:21:03 +0100

    SVD Kalman Filter

---
+++ Wang_Libert_Manneback_1992/svd_kalman_filter.jl
@@ -0,0 +1,12 @@
+#------------------------------------------------------------------------------
+# Julia Code for a SVD-Kalman-Filter
+# Wang, L., Libert, G. & Manneback, P. (1992).
+# Kalman Filter Algorithm Based on Singualar Value Decomposition.
+#------------------------------------------------------------------------------
+using LinearAlgebra
+using CSV
+using Plots
+using Statistics
+using Distributions
+using JuliaInterpreter
+using Infiltrator
