a4ea066cbf6c74be6072702cf235f9d4d58030f7
Kai Jennissen <kai.jennissen@statworx.com>
2020-03-10 17:22:03 +0100

    Particle Filter

--- Petris_Petrone_Campagnoli_2009/particle_filter.R
+++ Petris_Petrone_Campagnoli_2009/particle_filter.R
@@ -71,15 +71,15 @@ particle_filter <- function(y, N, mod) {
   
   TT <- length(y)
   q <- dim(W)[1]
-  theta_pf <- array(NA_real_, c(q, N, TT + 1))
-  wt <- matrix(NA_real_, N)
+  thetaPF <- array(NA_real_, c(q, TT + 1, N))
+  wt <- matrix(NA_real_, TT+1, N)
   m0 <- m0(mod) 
   C0 <- C0(mod) 
   L0 <- chol(C0)
 
   # 0
-  theta_pf[, , 1] <- m0 + L0 %*% matrix(rnorm(q*N), nrow = q) # initial draws
-  wt[] <- 1 / N
+  thetaPF[, 1, ] <- m0 + L0 %*% matrix(rnorm(q*N), nrow = q) # initial draws
+  wt[1, ] <- 1 / N
   
   N_0 <-  0.5 * N
   
@@ -103,53 +103,54 @@ particle_filter <- function(y, N, mod) {
     # b) optimal importance kernel
     # draw from Normal
     mu_hat <-
-      (GG %*% theta_pf[, , t - 1]  + W %*% t(FF) %*%
-         Sigma_inv %*% (y[t - 1] - FF %*% GG %*% theta_pf[, , t - 1]))
-    theta_pf[, , t] <-
+      (GG %*% thetaPF[, t - 1, ]  + W %*% t(FF) %*%
+         Sigma_inv %*% (y[t - 1] - FF %*% GG %*% thetaPF[ , t - 1, ]))
+    thetaPF[, t, ] <-
       mu_hat + C %*% matrix(rnorm(N * q), nrow = 2)
     log_target <-
       (
         dnorm(
           x = y[t - 1],
-          mean = c(FF %*% theta_pf[, , t]),
+          mean = c(FF %*% thetaPF[, t, ]),
           sd = c(V),
           log = TRUE
         ) +
           vec_log_pdf_mvnorm(
-            x = theta_pf[, , t],
-            mu = GG %*% theta_pf[, , t - 1],
+            x = thetaPF[, t, ],
+            mu = GG %*% thetaPF[ , t - 1, ],
             Sigma = W
           ) -
           vec_log_pdf_mvnorm(
-            x = theta_pf[, , t],
+            x = thetaPF[, t, ],
             mu = mu_hat,
             Sigma = tcrossprod(L)
           )
       )
     
-    wt <- wt * exp(log_target)
+    wt[t, ] <- wt[t-1, ] * exp(log_target)
     
     # 1.2
-    wt <- wt / sum(wt)
+    wt[t, ] <- wt[t, ]  / sum(wt[t, ])
     
     # 1.3
-    N_eff <- 1 / crossprod(wt)
+    N_eff <- 1 / crossprod(wt[t, ])
     
     # 1.4
-    if (N_eff < N_0  & N_eff > 5 * N_0) {
+    #browser()
+    if (N_eff < N_0 ) {
       idx <- sample(x = N,
                     N,
                     replace = TRUE,
-                    prob = wt[, 1])
-      theta_pf[, , t] <- theta_pf[, idx, t]
-      wt[] <- 1 / N
+                    prob = wt[t, ])
+      thetaPF[, t, ] <- thetaPF[, t, idx]
+      wt[t, ] <- 1 / N
     }
   }
-  return(theta_pf)
+  return(list(theta=thetaPF, wt=wt))
 }
 
 # Simulate ----------------------------------------------------------------
-n <- 200
+n <- 100
 mod <-
   dlmModPoly(
     2,
@@ -163,29 +164,87 @@ simData <- dlmForecast(mod, nAhead = n, sampleNew = 1)
 y <- simData$newObs[[1]]
 plot.ts(y)
 
-
 # Particle Filter
-mod_pf <-
-  dlmModPoly(
-    2,
-    dV = 1,
-    dW = c(1, 1),
-    m0 = c(100, 0.05),
-    C0 = diag(c(100, 100))
-  )
-theta_pf <- particle_filter(y, 2000, mod_pf)
-theta_pf_hat <- t(apply(theta_pf, MARGIN = c(1, 3), FUN = mean))
+# mod_pf <-
+#   dlmModPoly(
+#     2,
+#     dV = 1,
+#     dW = c(1, 1),
+#     m0 = c(100, 0.05),
+#     C0 = diag(c(100, 100))
+#   )
+resuPF <- particle_filter(y, 1000, mod)
+thetaPF <- resuPF$theta[, -1, ]
+wt <- resuPF$wt[-1,  ]
+thetaHatPF <- t(sapply(1:TT, FUN = function(i){thetaPF[, i, ]%*%wt[i, ]} ))
 
 # Kalman Filter
 y_filt <- dlmFilter(y, mod)
-theta_kf <- y_filt$m
+thetaHatKF <- y_filt$m[-1, ]
 
 # Plot
 plot.ts(
   cbind(c(y), # observed
-        theta_pf_hat[-1, 1], # particle filter
+        thetaHatPF[, 1], # particle filter
         #theta_gibbs_hat[1, -1],
-        theta_kf[-1, 1]),
+        thetaHatKF[, 1]),
   plot.type = "single",
   col = c("red", "blue", "darkgreen")
 )
+
+
+
+# Petris ------------------------------------------------------------------
+mod <- dlmModPoly(1, dV=2, dW=1, m0=10, C0=9)
+n <- 100
+set.seed(23)
+simData <- dlmForecast(mod, nAhead = n, sampleNew = 1)
+y <- simData$newObs[[1]]
+plot.ts(y)
+
+# particle filter
+N <- 1000
+N_0 <- N/2
+pfOut <- matrix(NA_real_, n+1, N)
+wt <- matrix(NA_real_, n+1, N)
+importanceSD <- sqrt(drop(W(mod) - W(mod)^2 / (W(mod)+V(mod))))
+predSd <- sqrt(drop(W(mod) + V(mod)))
+
+pfOut[1, ] <- rnorm(N, mean=m0(mod), sd=sqrt(C0(mod)))
+wt[1, ] <- rep(1/N, N)
+for (it in 2:(n+1)){
+  means <- pfOut[it-1, ] + drop(W(mod)) * (y[it-1] -pfOut[it-1, ]) / drop(W(mod) + V(mod))
+  pfOut[it, ] <- rnorm(N, mean = means, sd = importanceSD)
+  wt[it, ] <- dnorm(y[it-1], mean = pfOut[it-1, ], sd =  predSd) * wt[it-1, ]
+  
+  wt[it, ] <- wt[it, ] / sum(wt[it, ])
+  N.eff <- 1/crossprod(wt[it, ])
+if (N.eff < N_0){
+  
+  index <- sample(N, N, replace=TRUE, prob = wt[it, ])
+  pfOut[it, ] <- pfOut[it, index]
+  wt[it, ] <- 1/N
+  }
+}
+
+
+
+modFilt <- dlmFilter(y, mod)
+thetaHatKF <- modFilt$m[-1]
+sdKF <- with(modFilt, sqrt(unlist(dlmSvd2var(U.C, D.C))))[-1]
+pfOut <- pfOut[-1, ]
+wt <- wt[-1, ]
+thetaHatPF <- sapply(1:n, function(i){
+  weighted.mean(pfOut[i, ], wt[i, ])
+})
+sdPF <- sapply(1:n, function(i){
+  sqrt(weighted.mean((pfOut[i, ] - thetaHatPF[i])^2, wt[i, ]))
+})
+
+plot.ts(cbind(thetaHatKF, thetaHatPF), plot.type = "s", lty = c("dotted", "longdash"), 
+        xlab="", ylab=expression(m[t]))
+legend("topleft", c("Kalman", "Particle"), lty=c("dotted", "longdash"), bty = "n")
+
+plot.ts(cbind(sdKF, sdPF), plot.type = "s", lty = c("dotted", "longdash"), 
+        xlab="", ylab=expression(sqrt(C[t])))
+legend("topleft", c("Kalman", "Particle"), lty=c("dotted", "longdash"), bty = "n")
